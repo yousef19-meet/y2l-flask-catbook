a6af66d97dd397cd471f41e685e2c8211563688f
Subhi Beidas <subhi@goshippo.com>
2018-11-08 00:28:00 +0200

    added sample data to db

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
+from database import get_all_cats
 
 app = Flask(__name__)
 
 @app.route('/')
 def catbook_home():
-    return render_template("home.html")
+    cats = get_all_cats()
+    return render_template("home.html", cats=cats)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- database.py
+++ database.py
@@ -12,3 +12,7 @@ def create_cat(name):
     cat_object = Cat(name=name)
     session.add(cat_object)
     session.commit()
+
+def get_all_cats():
+    cats = session.query(Cat).all()
+    return cats
