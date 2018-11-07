78abdc7e7b14a45715883e0b7eea4b782d78fe9e
Subhi Beidas <subhi@goshippo.com>
2018-11-07 02:44:21 +0200

    Sample catbooks app

---
+++ database.py
@@ -0,0 +1,14 @@
+from models import *
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///cats.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def create_cat(name):
+    cat_object = Cat(name=name)
+    session.add(cat_object)
+    session.commit()
---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Cat(Base):
+    __tablename__ = "cats"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    
