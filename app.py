013ca91f9bc306994f22f9eba3eb35dc523519f1
yousef19-meet <yousef19@meet.mit.edu>
2018-11-15 08:41:08 -0800

    nothing done..

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
 
+@app.route('/cats/<string:cat_view>', methods = ['GET'])
+def catbook(cat_view):
+    cats = get_all_cats()
+    # cat = get_cat(id)
+    return render_template("cat.html", cats=cats)
 if __name__ == '__main__':
    app.run(debug = True)
